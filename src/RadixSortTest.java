6c028e8b5bb889f41536ab53914f19d32d716526
Alvaco <vas09284@uvg.edu.gt>
2017-02-14 00:21:51 -0600

    Prueba Unitaria RadixSort

---
+++ src/RadixSortTest.java
@@ -0,0 +1,32 @@
+/**
+ * RadixSortTest.java
+ * Universidad del Valle de Guatemala
+ * Algoritmos y Estructura de Datos Seccion 10
+ * Hoja de Trabajo 3
+ * Christopher Sandoval 13660
+ * Fernanda Estrada 14198
+ * Ana Lucia Diaz 151378
+ * Alejandro Vasquez 09284
+ * @since 2/13/2017
+ * Tomado de https://en.wikibooks.org/wiki/Algorithm_Implementation/Sorting/Gnome_sort
+ */
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class RadixSortTest {
+
+	@Test
+	public void testSort() {
+		RadixSort test = new RadixSort();
+		int[] arrayTest = new int[4];
+		for (int i = 0; i<4; i++){
+			arrayTest[i] = 4-i;
+		}
+		arrayTest = test.sort(arrayTest);
+		assertArrayEquals(new int[]{1,2,3,4}, arrayTest);
+	}
+
+}
